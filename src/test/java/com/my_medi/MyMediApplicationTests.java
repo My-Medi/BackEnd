638814231e396ea1f3195f4fa69a1c6f0d0d7666
hann <wjdgks1233@gmail.com>
2025-06-03 19:13:58 +0900

    init commit

---
+++ src/test/java/com/my_medi/MyMediApplicationTests.java
@@ -0,0 +1,13 @@
+package com.my_medi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyMediApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
