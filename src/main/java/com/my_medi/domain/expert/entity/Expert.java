f44bf6c263ad865083de2148ab190fb2e57bd269
Pyun Seon a <139514905+Seona12@users.noreply.github.com>
2025-07-07 23:05:27 +0900

    [Medi 29] ERD design (Member/User/Expert) (#8)
    * [MEDI-29] member, expert entity base
    
    * [MEDI-29] user entity base
    
    * [MEDI-29] join delete
    
    * [MEDI-29] fix(erd) : ERD comment edit
    
    * [MEDI-29] fix(Member) : Column name edit
    
    * [MEDI-29] fix(discriminator) add staticvariable

--- src/main/java/com/my_medi/domain/expert/entity/Expert.java
+++ src/main/java/com/my_medi/domain/expert/entity/Expert.java
@@ -1,21 +1,50 @@
 package com.my_medi.domain.expert.entity;
 
+import com.my_medi.common.consts.StaticVariable;
 import com.my_medi.domain.member.entity.Member;
-import jakarta.persistence.DiscriminatorValue;
-import jakarta.persistence.Entity;
+import com.my_medi.domain.member.entity.Role;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
+import java.util.ArrayList;
+
 @Entity
 @Getter
 @SuperBuilder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
-@DiscriminatorValue("type_expert")      //TODO ROLE ENUM key값으로
+@DiscriminatorValue(StaticVariable.EXPERT)      //TODO ROLE ENUM key값으로
 public class Expert extends Member {
 
+    @Column(unique = true)
     private String expertUuid;
+
+    //전문분야(enum)
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Specialty specialty;
+
+    //소속 회사 기관명
+    private String organizationName;
+
+    // 자격증
+    // 데이터베이스에 입력될 텍스트 크기를 길게 설정
+    @Column(length = 1000)
+    private String licenseFileUrl;
+
+    // 경력사항(추후 연관관계로 받아올 예정)
+    // @OneToMany(mappedBy = "expert", cascade = CascadeType.ALL, orphanRemoval = true)
+    // private List<경력테이블> careers = new ArrayList<>();
+
+    //경력소개
+    // 데이터베이스에 입력될 텍스트 크기를 길게 설정
+    @Column(columnDefinition = "TEXT")
+    private String introduction;
+
+
+
 }
