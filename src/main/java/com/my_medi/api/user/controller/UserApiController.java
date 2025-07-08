ce532c96215e1ffae8f7edd4c940ec2ec6fc8eb4
hann <wjdgks1233@gmail.com>
2025-07-08 12:15:11 +0900

    [MEDI-33] feat : create example exception of user

--- src/main/java/com/my_medi/api/user/controller/UserApiController.java
+++ src/main/java/com/my_medi/api/user/controller/UserApiController.java
@@ -1,9 +1,11 @@
 package com.my_medi.api.user.controller;
 
 import com.my_medi.domain.user.entity.User;
+import com.my_medi.domain.user.exception.UserHandler;
 import com.my_medi.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,16 +15,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class UserApiController {
 
-    //지금은 테스트를 위해 레포지토리 의존성 주입을 해둔 상태입니다
-    private final UserRepository userRepository;
 
     //TODO remove this method
-    @Transactional
-    @PostMapping
-    public Long registerUserForTest() {
-        return userRepository.save(User
-                .builder()
-                .userUuid("test")
-                .build()).getId();
+    @GetMapping
+    public void registerUserForTest() {
+        throw UserHandler.NOT_FOUND;
     }
 }
