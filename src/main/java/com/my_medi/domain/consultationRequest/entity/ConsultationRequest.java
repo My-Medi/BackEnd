bc7a19ad87cdd7911bd0a5d17d352f7c86835bbb
Sohyunnnn <thgus05061@naver.com>
2025-07-09 21:57:50 +0900

    [MEDI-36] feat: create ConsultationRequest entity

---
+++ src/main/java/com/my_medi/domain/consultationRequest/entity/ConsultationRequest.java
@@ -0,0 +1,35 @@
+package com.my_medi.domain.consultationRequest.entity;
+
+import com.my_medi.domain.expert.entity.Expert;
+import com.my_medi.domain.model.entity.BaseTimeEntity;
+import com.my_medi.domain.user.entity.User;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "consultation_request")
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ConsultationRequest extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "request_status", nullable = false)
+    private RequestStatus requestStatus;
+
+    @Column(columnDefinition = "TEXT")
+    private String comment;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "expert_id", nullable = false)
+    private Expert expert;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+}
