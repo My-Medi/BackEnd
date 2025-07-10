178a8ccaffcffca249b478adff55f7a001a3a335
hann <wjdgks1233@gmail.com>
2025-07-10 15:33:08 +0900

    [MEDI-37] feat: create interface of user service

---
+++ src/main/java/com/my_medi/domain/user/service/UserCommandService.java
@@ -0,0 +1,14 @@
+package com.my_medi.domain.user.service;
+
+import com.my_medi.api.member.dto.RegisterMemberDto;
+
+public interface UserCommandService {
+
+    //TODO [LATER] requestDto에 맞게 argument 변경해주기
+    Long registerUser(RegisterMemberDto registerMemberDto);
+
+    //TODO [Now] 변경할 값들 argument로 받아오기 : dto 직접 생성
+    Long updateUserInformation(Long userId);
+
+    void deleteUserAccount(Long userId);
+}
---
+++ src/main/java/com/my_medi/domain/user/service/UserQueryService.java
@@ -0,0 +1,12 @@
+package com.my_medi.domain.user.service;
+
+import com.my_medi.domain.user.entity.User;
+
+import java.util.List;
+
+public interface UserQueryService {
+
+    User getUserById(Long expertId);
+
+    List<User> getAllUsers();
+}
