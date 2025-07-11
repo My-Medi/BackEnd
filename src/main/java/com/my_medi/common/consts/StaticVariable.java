525123ee34e0f392f2480fbcaca325cdbc410344
JEONGHAN <69452755+H4nnhoi@users.noreply.github.com>
2025-07-11 11:25:54 +0900

    [MEDI-32] swagger setting (#19)
    * [MEDI-33] feat : about exception classes
    
    * [MEDI-33] feat : create ExceptionAdvice.class
    
    * [MEDI-33] feat : create example exception of user
    
    * [MEDI-32] feat : setting swagger
    
    * [MEDI-32] refactor : complete swagger setting
    
    * [MEDI-32] refactor : setting build.gradle

--- src/main/java/com/my_medi/common/consts/StaticVariable.java
+++ src/main/java/com/my_medi/common/consts/StaticVariable.java
@@ -3,4 +3,10 @@ package com.my_medi.common.consts;
 public class StaticVariable {
     public static final String USER = "type_user";
     public static final String EXPERT = "type_expert";
+    public static final String SWAGGER_JWT = "JWT";
+    public static final String SWAGGER_BEARER = "Bearer";
+    public static final String BEARER = "Bearer ";
+    public static final String AUTHORIZATION = "Authorization";
+    public static final String GRANT_TYPE = "authorization_code";
 }
+
