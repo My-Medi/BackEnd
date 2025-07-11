acb71d755b2ba550cf211b942dfc472485157a16
JEONGHAN <69452755+H4nnhoi@users.noreply.github.com>
2025-07-11 11:25:54 +0900

    [MEDI-32] swagger setting (#19)
    * [MEDI-33] feat : about exception classes
    
    * [MEDI-33] feat : create ExceptionAdvice.class
    
    * [MEDI-33] feat : create example exception of user
    
    * [MEDI-32] feat : setting swagger
    
    * [MEDI-32] refactor : complete swagger setting
    
    * [MEDI-32] refactor : setting build.gradle

--- src/main/java/com/my_medi/api/user/controller/UserApiController.java
+++ src/main/java/com/my_medi/api/user/controller/UserApiController.java
@@ -3,6 +3,8 @@ package com.my_medi.api.user.controller;
 import com.my_medi.domain.user.entity.User;
 import com.my_medi.domain.user.exception.UserHandler;
 import com.my_medi.domain.user.repository.UserRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,15 +12,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "사용자 API")
 @RestController
 @RequestMapping("/api/v1/users")
 @RequiredArgsConstructor
 public class UserApiController {
 
-
-    //TODO remove this method
-    @GetMapping
-    public void registerUserForTest() {
-        throw UserHandler.NOT_FOUND;
-    }
 }
