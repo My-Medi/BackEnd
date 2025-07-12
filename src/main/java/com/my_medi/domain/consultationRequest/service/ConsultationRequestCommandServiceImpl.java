556a6a194da0722851b457aa3bb244b465689042
Sohyunnnn <thgus05061@naver.com>
2025-07-12 14:38:42 +0900

    [MEDI-39] refactor: refactor ConsultationRequest service

--- src/main/java/com/my_medi/domain/consultationRequest/service/ConsultationRequestCommandServiceImpl.java
+++ src/main/java/com/my_medi/domain/consultationRequest/service/ConsultationRequestCommandServiceImpl.java
@@ -6,7 +6,8 @@ import com.my_medi.domain.consultationRequest.repository.ConsultationRequestRepo
 import com.my_medi.domain.expert.entity.Expert;
 import com.my_medi.domain.expert.repository.ExpertRepository;
 import com.my_medi.domain.member.entity.Member;
-import com.my_medi.domain.member.repository.MemberRepository;
+import com.my_medi.domain.user.entity.User;
+import com.my_medi.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,12 +18,12 @@ import org.springframework.transaction.annotation.Transactional;
 public class ConsultationRequestCommandServiceImpl implements ConsultationRequestCommandService {
 
     private final ConsultationRequestRepository consultationRequestRepository;
-    private final MemberRepository memberRepository;
+    private final UserRepository userRepository;
     private final ExpertRepository expertRepository;
 
     @Override
     public Long requestConsultationToExpert(Long userId, Long expertId, String comment) {
-        Member user = memberRepository.findById(userId)
+        User user = userRepository.findById(userId)
                 .orElseThrow(() -> new IllegalArgumentException("User not found"));
         Expert expert = expertRepository.findById(expertId)
                 .orElseThrow(() -> new IllegalArgumentException("Expert not found"));
@@ -31,7 +32,7 @@ public class ConsultationRequestCommandServiceImpl implements ConsultationReques
                 .user(user)
                 .expert(expert)
                 .comment(comment)
-                .status(RequestStatus.PENDING)
+                .requestStatus(RequestStatus.REQUESTED)
                 .build();
 
         return consultationRequestRepository.save(request).getId();
@@ -51,6 +52,6 @@ public class ConsultationRequestCommandServiceImpl implements ConsultationReques
         ConsultationRequest request = consultationRequestRepository.findById(consultationRequestId)
                 .orElseThrow(() -> new IllegalArgumentException("Consultation request not found"));
 
-        request.cancel(); // 내부에서 status를 CANCELED로 바꾸는 메서드
+        consultationRequestRepository.delete(request);
     }
 }
