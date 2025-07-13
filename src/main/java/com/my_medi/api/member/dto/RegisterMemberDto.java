f0db0af6da8bcb98113537400d063475191e80f8
Pyun Seon a <139514905+Seona12@users.noreply.github.com>
2025-07-13 23:34:32 +0900

    [MEDI-42] controller of user&expert (#23)
    * [MEDI-37] feat: create interface of expert service
    
    * [MEDI-37] feat: create interface of user service
    
    * [MEDI-37] dto, service logic
    
    * [MEDI-37] fix : Rename parameter
    
    * [MEDI-37] exception logic
    
    * [MEDI-37] fix : Transactional
    
    * [MEDI-37] fix : registerExpert, registerUser
    
    * [MEDI-42] feat : create UserApiController
    
    * [MEDI-39] business logic interface of consultation schedule (#18)
    
    * [MEDI-37] feat: create interface of expert service
    
    * [MEDI-37] feat: create interface of user service
    
    * [MEDI-39] feat : create interfaces of schedule service
    
    * [MEDI-37] dto, service logic
    
    * [MEDI-37] fix : Rename parameter
    
    * [MEDI-37] exception logic
    
    * [MEDI-39] feat : create service interface of consultationRequest
    
    * [MEDI-39] feat: implement ConsultationRequest and Schedule
    
    * [MEDI-37] fix : Transactional
    
    * [MEDI-39] refactor: refactor errorStatus
    
    * [MEDI-39] refactor: refactor schedule service
    
    * [MEDI-39] refactor: refactor ConsultationRequest service
    
    * [MEDI-39] chore: remove unused import
    
    * [MEDI-39] chore: temporarily comment out
    
    * [MEDI-39] feat: implement editSchedule logic
    
    * [MEDI-39] minor fixes
    
    * [MEDI-39] refactor: refactor schedule error handler
    
    * [MEDI-39] feat: implement consultation request error handler
    
    * [MEDI-39] refactor: reformat code style
    
    ---------
    
    Co-authored-by: pyun seona <vustjsdk1@naver.com>
    Co-authored-by: Sohyunnnn <thgus05061@naver.com>
    
    * [MEDI-37] business logic interface of user expert (#16)
    
    * [MEDI-37] feat: create interface of expert service
    
    * [MEDI-37] feat: create interface of user service
    
    * [MEDI-37] dto, service logic
    
    * [MEDI-37] fix : Rename parameter
    
    * [MEDI-37] exception logic
    
    * [MEDI-37] fix : Transactional
    
    * [MEDI-37] fix : registerExpert, registerUser
    
    * [MEDI-37] fix : username
    
    * [MEDI-37] fix : Member Entity username add
    
    * [MEDI-37] fix : username uuid edit
    
    ---------
    
    Co-authored-by: pyun seona <vustjsdk1@naver.com>
    Co-authored-by: Pyun Seon a <139514905+Seona12@users.noreply.github.com>
    
    * [MEDI-42] user register api
    
    * [MEDI-42] dto created
    
    * [MEDI-42] fix : comment fix
    
    * [MEDI-42] add UserCommandServiceImpl height weight
    
    ---------
    
    Co-authored-by: hann <wjdgks1233@gmail.com>
    Co-authored-by: JEONGHAN <69452755+H4nnhoi@users.noreply.github.com>
    Co-authored-by: Sohyunnnn <thgus05061@naver.com>

---
+++ src/main/java/com/my_medi/api/member/dto/RegisterMemberDto.java
@@ -0,0 +1,22 @@
+package com.my_medi.api.member.dto;
+
+import com.my_medi.domain.member.entity.Gender;
+import com.my_medi.domain.member.entity.Role;
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+@Builder
+public class RegisterMemberDto {
+    private String name;
+    private LocalDate birthDate;
+    private Gender gender;
+    private String nickname;
+    private String email;
+    private String phoneNumber;
+    private String profileImgUrl;
+
+
+}
---
+++ src/main/java/com/my_medi/domain/user/service/UserCommandServiceImpl.java
@@ -0,0 +1,57 @@
+package com.my_medi.domain.user.service;
+import com.my_medi.api.member.dto.RegisterMemberDto;
+import com.my_medi.api.user.dto.RegisterUserDto;
+import com.my_medi.domain.expert.entity.Expert;
+import com.my_medi.domain.member.entity.Role;
+import com.my_medi.domain.user.dto.UpdateUserDto;
+import com.my_medi.domain.user.entity.User;
+import com.my_medi.domain.user.repository.UserRepository;
+import com.my_medi.domain.user.exception.UserHandler;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class UserCommandServiceImpl implements UserCommandService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public Long registerUser(RegisterUserDto registerUserDto){
+        //TODO [LATER] requestDto에 맞게 구현예정
+        User user = User.builder()
+                .name(registerUserDto.getMember().getName())
+                .birthDate(registerUserDto.getMember().getBirthDate())
+                .gender(registerUserDto.getMember().getGender())
+                .username(UUID.randomUUID().toString())
+                .email(registerUserDto.getMember().getEmail())
+                .phoneNumber(registerUserDto.getMember().getPhoneNumber())
+                .profileImgUrl(registerUserDto.getMember().getProfileImgUrl())
+                .role(Role.USER)
+                .height(registerUserDto.getHeight())
+                .weight(registerUserDto.getWeight())
+                .build();
+        return userRepository.save(user).getId();
+    }
+
+    @Override
+    public Long updateUserInformation(Long userId, UpdateUserDto dto) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> UserHandler.NOT_FOUND);
+        user.modifyUserInfo(dto);
+        return user.getId();
+    }
+
+    @Override
+    public void deleteUserAccount(Long userId) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> UserHandler.NOT_FOUND);
+        userRepository.delete(user); // Hard delete
+    }
+
+}
+
