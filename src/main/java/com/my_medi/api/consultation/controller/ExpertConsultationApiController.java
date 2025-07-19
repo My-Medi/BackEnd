934e767b4af6d9f3fb21da96d7356ea2f9eba0ed
Sohyunnnn <thgus05061@naver.com>
2025-07-19 14:02:05 +0900

    [MEDI-43] feat: implement GET API for consultation request

--- src/main/java/com/my_medi/api/consultation/controller/ExpertConsultationApiController.java
+++ src/main/java/com/my_medi/api/consultation/controller/ExpertConsultationApiController.java
@@ -1,14 +1,16 @@
 package com.my_medi.api.consultation.controller;
 
 import com.my_medi.api.common.dto.ApiResponseDto;
+import com.my_medi.domain.consultationRequest.entity.ConsultationRequest;
+import com.my_medi.domain.consultationRequest.entity.RequestStatus;
 import com.my_medi.domain.consultationRequest.service.ConsultationRequestCommandService;
+import com.my_medi.domain.consultationRequest.service.ConsultationRequestQueryService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Tag(name = "전문가 상담요청 API")
 @RestController
@@ -17,20 +19,37 @@ import org.springframework.web.bind.annotation.RestController;
 public class ExpertConsultationApiController {
 
     private final ConsultationRequestCommandService consultationRequestCommandService;
+    private final ConsultationRequestQueryService consultationRequestQueryService;
 
         @Operation(summary = "consult request 승낙")
         @PatchMapping("/{consultationId}/approve")
             public ApiResponseDto<Long> approveConsultation(@PathVariable Long consultationId) {
                 consultationRequestCommandService.approveConsultation(consultationId);
                 return ApiResponseDto.onSuccess(consultationId);
+            }
 
-    }
+        @Operation(summary = "consult request 거절")
+        @PatchMapping("/{consultationId}/reject")
+        public ApiResponseDto<Long> rejectConsultation(@PathVariable Long consultationId) {
+            consultationRequestCommandService.rejectConsultation(consultationId);
+            return ApiResponseDto.onSuccess(consultationId);
+        }
 
-    @Operation(summary = "consult request 거절")
-    @PatchMapping("/{consultationId}/reject")
-    public ApiResponseDto<Long> rejectConsultation(@PathVariable Long consultationId) {
-        consultationRequestCommandService.rejectConsultation(consultationId);
-        return ApiResponseDto.onSuccess(consultationId);
+    @Operation(summary = "자신에게 들어온 상담 요청 목록 조회")
+    @GetMapping
+    public ApiResponseDto<List<ConsultationRequest>> getConsultationRequests(
+            @RequestParam Long expertId,
+            @RequestParam(required = false) RequestStatus status
+    ) {
+        List<ConsultationRequest> requests;
+
+        if (status != null) {
+            requests = consultationRequestQueryService.getRequestByExpert(expertId, status);
+        } else {
+            requests = consultationRequestQueryService.getAllRequestByExpert(expertId);
+        }
+
+        return ApiResponseDto.onSuccess(requests);
     }
 
 }
