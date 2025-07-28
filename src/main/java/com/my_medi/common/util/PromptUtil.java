64f31f67089ae288b8835593be33fe25e07d89b0
JEONGHAN <69452755+H4nnhoi@users.noreply.github.com>
2025-07-28 14:09:29 +0900

    [MEDI-48] open ai setting (#37)
    * [MEDI-48] feat : create OpenAiService.class and add business logic
    
    * [MEDI-48] rename : rename service of open ai
    
    * [MEDI-48] feat : create api of parsing img and deserialization data
    
    * [MEDI-48] fix : resolve error of using ai
    
    * [MEDI-48] refactor : create Prompt.class for clean code in extractTextFromImage
    
    * [MEDI-48] refactor : use consts for clean code
    
    * [MEDI-48] refactor : feedback by review

---
+++ src/main/java/com/my_medi/common/util/PromptUtil.java
@@ -0,0 +1,43 @@
+package com.my_medi.common.util;
+
+import com.my_medi.common.consts.Prompt;
+import com.my_medi.common.consts.StaticVariable;
+import com.my_medi.infra.gpt.dto.OpenAIRequest;
+import com.my_medi.infra.gpt.dto.OpenAIResponse;
+import org.springframework.http.*;
+
+import java.util.List;
+
+import static com.my_medi.common.consts.Prompt.*;
+
+public class PromptUtil {
+    public static OpenAIRequest extractTextFromImage(String imageBase64) {
+        String prompt = Prompt.IMAGE_PARSING_PROMPT;
+
+        OpenAIRequest.Content textContent = OpenAIRequest.Content.builder()
+                .type(TEXT)
+                .text(prompt)
+                .build();
+
+        OpenAIRequest.Content imageContent = OpenAIRequest.Content.builder()
+                .type(TYPE_IMAGE_URL)
+                .imageUrl(
+                        OpenAIRequest.ImageUrl.builder()
+                                .url(IMAGE_URL_URL + imageBase64)
+                                .build()
+                )
+                .build();
+
+        OpenAIRequest.Message message = OpenAIRequest.Message.builder()
+                .role(ROLE_USER)
+                .content(List.of(textContent, imageContent))
+                .build();
+
+        return OpenAIRequest.builder()
+                .model(MODEL_GPT_4O)
+                .maxTokens(MAX_TOKEN)
+                .temperature(TEMPERATURE)
+                .messages(List.of(message))
+                .build();
+    }
+}
---
+++ src/main/java/com/my_medi/infra/gpt/service/OpenAIServiceImpl.java
@@ -0,0 +1,75 @@
+package com.my_medi.infra.gpt.service;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.my_medi.common.consts.StaticVariable;
+import com.my_medi.infra.gpt.dto.HealthReportData;
+import com.my_medi.infra.gpt.dto.OpenAIRequest;
+import com.my_medi.infra.gpt.dto.OpenAIResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.env.Environment;
+import org.springframework.http.*;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import static com.my_medi.common.consts.StaticVariable.*;
+import static com.my_medi.common.util.ParseUtil.*;
+import static com.my_medi.common.util.PromptUtil.extractTextFromImage;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class OpenAIServiceImpl implements OpenAIService {
+
+    private final RestTemplate restTemplate;
+    private final ObjectMapper objectMapper;
+    private final Environment environment;
+
+    @Override
+    public HealthReportData parseHealthReport(String imageBase64) {
+        //GET ENV value
+        String openAiApiKey = environment.getProperty("openai.api.key");
+        String openAiApiUrl = environment.getProperty("openai.api.url");
+        //EXTRACT by prompt
+        OpenAIRequest openAIRequest = extractTextFromImage(imageBase64);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.setBearerAuth(openAiApiKey);
+        HttpEntity<OpenAIRequest> entity = new HttpEntity<>(openAIRequest, headers);
+        ResponseEntity<OpenAIResponse> response = restTemplate.exchange(
+                openAiApiUrl, HttpMethod.POST, entity, OpenAIResponse.class);
+
+        return parseExtractedData(response.getBody()
+                .getChoices().get(0)
+                .getMessage()
+                .getContent());
+    }
+
+    private HealthReportData parseExtractedData(String jsonResponse) {
+        try {
+            // JSON에서 실제 JSON 부분만 추출
+            int startIndex = jsonResponse.indexOf("{");
+            int endIndex = jsonResponse.lastIndexOf("}") + 1;
+            String jsonPart = jsonResponse.substring(startIndex, endIndex);
+
+            JsonNode jsonNode = objectMapper.readTree(jsonPart);
+
+            return HealthReportData.builder()
+                    .checkupDate(parseDate(jsonNode.path(CHECKUP_DATE).asText()))
+                    .round(jsonNode.path(ROUND).asInt())
+                    .measurement(parseMeasurement(jsonNode.path(MEASUREMENT)))
+                    .bloodPressure(parseBloodPressure(jsonNode.path(BLOOD_PRESSURE)))
+                    .bloodTest(parseBloodTest(jsonNode.path(BLOOD_TEST)))
+                    .urineTest(parseUrineTest(jsonNode.path(URINE_TEST)))
+                    .imagingTest(parseImagingTest(jsonNode.path(IMAGING_TEST)))
+                    .interview(parseInterview(jsonNode.path(INTERVIEW)))
+                    .additionalTest(parseAdditionalTest(jsonNode.path(ADDITIONAL_TEST)))
+                    .build();
+        } catch (Exception e) {
+            //TODO exception 처리
+            log.error("JSON 파싱 중 오류 발생: {}", jsonResponse, e);
+            throw new RuntimeException("추출된 데이터 파싱에 실패했습니다.", e);
+        }
+    }
+}
