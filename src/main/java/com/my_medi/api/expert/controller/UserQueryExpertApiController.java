062efe99d23f1bfde496365c613c01cb9ea3fc55
hann <wjdgks1233@gmail.com>
2025-08-01 16:03:43 +0900

    [MEDI-74] feature : create API of getAllExpertProfile by pagination

--- src/main/java/com/my_medi/api/expert/controller/UserQueryExpertApiController.java
+++ src/main/java/com/my_medi/api/expert/controller/UserQueryExpertApiController.java
@@ -5,16 +5,17 @@ import com.my_medi.api.consultation.validator.ExpertAllowedToViewUserInfoValidat
 import com.my_medi.api.expert.dto.ExpertResponseDto;
 import com.my_medi.api.expert.mapper.ExpertConverter;
 import com.my_medi.common.annotation.AuthUser;
+import com.my_medi.common.annotation.RequestParamList;
 import com.my_medi.domain.expert.entity.Expert;
 import com.my_medi.domain.expert.service.ExpertQueryService;
 import com.my_medi.domain.user.entity.User;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.annotation.Nullable;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Tag(name = "[사용자 페이지] 전문가 조회 API")
 @RestController
@@ -39,8 +40,14 @@ public class UserQueryExpertApiController {
         return ApiResponseDto.onSuccess(result);
     }
 
-//    @Operation(summary = "전문가 목록을 조회합니다. 이때 15개씩 등록 순으로 조회합니다.")
-//    @GetMapping("/experts")
+    //TODO 인가처리
+    @Operation(summary = "전문가 목록을 조회합니다. 이때 15개씩 등록 순으로 조회합니다.")
+    @GetMapping("/experts")
+    public ApiResponseDto<ExpertResponseDto.ExpertProfileListDto> getAllExpertProfile(
+            @Nullable @RequestParamList(value = "keyword") List<String> specialty
+    ) {
+        return ApiResponseDto.onSuccess(null);
+    }
 
 
 
