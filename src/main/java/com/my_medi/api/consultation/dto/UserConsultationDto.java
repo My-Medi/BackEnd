603b50050d56c0f560de764bf349806056081dc2
Sohyunnnn <thgus05061@naver.com>
2025-08-03 15:00:12 +0900

    [MEDI-76] refactor: modify dto

--- src/main/java/com/my_medi/api/consultation/dto/UserConsultationDto.java
+++ src/main/java/com/my_medi/api/consultation/dto/UserConsultationDto.java
@@ -1,13 +1,23 @@
 package com.my_medi.api.consultation.dto;
 
 import com.my_medi.domain.consultationRequest.entity.RequestStatus;
+import com.my_medi.domain.expert.entity.Specialty;
 import lombok.Builder;
 import lombok.Data;
+
+import java.time.LocalDate;
 
 @Data
 @Builder
 public class UserConsultationDto {
-    private Long id;
-    private String comment;
+    private Long consultationId;
+    private Long expertId;
     private RequestStatus status;
+    private Specialty specialty;
+    private String name;
+    private String nickname;
+    private String profileImgUrl;
+    private String phoneNumber;
+    private String introSentence;
+    private LocalDate requestDate;
 }
