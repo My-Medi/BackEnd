0e2636f9e419130d2b6b408bf405a21c4a82f348
hann <wjdgks1233@gmail.com>
2025-08-04 22:20:26 +0900

    [MEDI-66] feat : set api of create notification dummy

--- src/main/java/com/my_medi/api/common/controller/DummyApiController.java
+++ src/main/java/com/my_medi/api/common/controller/DummyApiController.java
@@ -4,10 +4,7 @@ import com.my_medi.api.common.dto.ApiResponseDto;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "더미 데이터 생성 API", description = "더미데이터 생성 API")
 @RestController
@@ -15,9 +12,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class DummyApiController {
 
-    @Operation(summary = "전문가 대량 생성 API")
+    @Operation(summary = "전문가 더미 생성 API")
     @PostMapping("/experts")
     public ApiResponseDto<String> createExpertsDummy(@RequestParam int count) {
+        return ApiResponseDto.onSuccess("success");
+    }
+
+    @Operation(summary = "알림 더미 생성 API", description = "send to user")
+    @PostMapping("/notifications/users/{userId}")
+    public ApiResponseDto<String> createNotificationToUserDummy(@PathVariable Long userId,
+                                                                @RequestParam int count) {
+        return ApiResponseDto.onSuccess("success");
+    }
+
+    @Operation(summary = "알림 더미 생성 API", description = "send to expert")
+    @PostMapping("/notifications/experts/{expertId}")
+    public ApiResponseDto<String> createNotificationToExpertDummy(@PathVariable Long expertId,
+                                                                  @RequestParam int count) {
         return ApiResponseDto.onSuccess("success");
     }
 }
