6d660cb07d12d00d4148d4e172297b856ea77552
pyun seona <vustjsdk1@naver.com>
2025-08-07 15:01:41 +0900

    [MEDI-88] fix responsedto

--- src/main/java/com/my_medi/api/user/dto/UserResponseDto.java
+++ src/main/java/com/my_medi/api/user/dto/UserResponseDto.java
@@ -13,8 +13,9 @@ public class UserResponseDto {
     @Builder
     public static class UserProfileDto{
         //TODO 채워넣기 유저랑 전문가 계정 등록하는 api 생성 jwt security 설정
-        private Long id;
+        private Long userid;
         private String name;
+        private String nickname;
         private String birthDate;
         private Gender gender;
         private String email;
