cfa6956eb16e5c29e31ee77c759eac54a6b2ceae
hann <wjdgks1233@gmail.com>
2025-08-11 23:01:45 +0900

    hotfix : add field in enum of report

--- src/main/java/com/my_medi/domain/report/enums/BloodPressureStatus.java
+++ src/main/java/com/my_medi/domain/report/enums/BloodPressureStatus.java
@@ -8,7 +8,8 @@ import lombok.RequiredArgsConstructor;
 public enum BloodPressureStatus {
     NORMAL("정상"),
     PREHYPERTENSION("고혈압 전단계"),
-    HYPERTENSION("고혈압 의심");
+    HYPERTENSION("고혈압 의심"),
+    HYPERTENSIVE_PATIENT("고혈압 유질환자");
 
     private final String key;
 }
