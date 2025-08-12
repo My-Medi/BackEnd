6446f89b39abcd01bb34f0edcdafab17df35d523
pyun seona <vustjsdk1@naver.com>
2025-08-12 17:51:21 +0900

    [MEDI-101] summary add

--- src/main/java/com/my_medi/api/healthTerm/controller/HealthTermApiController.java
+++ src/main/java/com/my_medi/api/healthTerm/controller/HealthTermApiController.java
@@ -3,6 +3,7 @@ package com.my_medi.api.healthTerm.controller;
 import com.my_medi.api.common.dto.ApiResponseDto;
 import com.my_medi.infra.gpt.dto.HealthTermResponse;
 import com.my_medi.infra.gpt.service.HealthTermService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,6 +20,7 @@ public class HealthTermApiController {
     private final HealthTermService healthTermService;
 
     // 예: GET /api/v1/users/term?term=콜레스테롤
+    @Operation(summary = "[건강용어 검색] 사용자가 건강용어를 검색하면 형식에 맞게 설명을 가져옵니다.")
     @GetMapping
     public ApiResponseDto<HealthTermResponse> getHealthTerm(@RequestParam String term) {
         return ApiResponseDto.onSuccess(healthTermService.define(term));
