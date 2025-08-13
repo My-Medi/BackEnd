33cf449e8067c0135ac317c8e2711c2098c14cfd
Dongwoo Go <kwcs_go670@naver.com>
2025-08-13 22:41:15 +0900

    [MEDI-105] feat: validate id and nickname is unique

--- src/main/java/com/my_medi/security/controller/TokenApiController.java
+++ src/main/java/com/my_medi/security/controller/TokenApiController.java
@@ -1,11 +1,11 @@
 package com.my_medi.security.controller;
 
 import com.my_medi.api.common.dto.ApiResponseDto;
+import com.my_medi.domain.member.repository.MemberRepository;
 import com.my_medi.security.jwt.dto.JwtToken;
 import com.my_medi.security.jwt.dto.MemberLoginRequestDto;
 import com.my_medi.security.jwt.service.TokenService;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -19,6 +19,7 @@ import org.springframework.web.bind.annotation.*;
 public class TokenApiController {
 
     private final TokenService tokenService;
+    private final MemberRepository memberRepository;
 
     @Operation(summary = "이메일로 JWT 토큰 발급")
     @PostMapping("/login")
@@ -30,5 +31,19 @@ public class TokenApiController {
     @PostMapping("/reissue")
     public ApiResponseDto<JwtToken> issueToken(@RequestParam String refresh) {
         return ApiResponseDto.onSuccess(tokenService.issueTokens(refresh));
+    }
+
+    @Operation(summary = "id unique 검증")
+    @PostMapping("/id")
+    public ApiResponseDto<Boolean> idValidator(@RequestParam String memberId) {
+        boolean isValid = !memberRepository.existsByLoginId(memberId);
+        return ApiResponseDto.onSuccess(isValid);
+    }
+
+    @Operation(summary = "nickname unique 검증")
+    @PostMapping("/nickname")
+    public ApiResponseDto<Boolean> nicknameValidator(@RequestParam String nickname) {
+        boolean isValid = !memberRepository.existsByNickname(nickname);
+        return ApiResponseDto.onSuccess(isValid);
     }
 }
