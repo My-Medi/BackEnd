d8b13bb22d3a614d86f899598b373fd14d7c9314
hann <wjdgks1233@gmail.com>
2025-08-15 00:08:07 +0900

    hotfix : error of cascade

--- src/main/java/com/my_medi/security/config/SecurityConfig.java
+++ src/main/java/com/my_medi/security/config/SecurityConfig.java
@@ -87,7 +87,8 @@ public class SecurityConfig {
                 "/api/v1/test/**",
                 "/api/v1/test/health-check",
                 "/api/v1/examples/user",
-                "/api/v1/examples/global"
+                "/api/v1/examples/global",
+                "/api/v1/tokens/**"
         };
     }
 
