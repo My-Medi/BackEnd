be3b17a0dcd9f4d19194e7596af20927cda037ce
Sohyunnnn <thgus05061@naver.com>
2025-08-20 20:12:48 +0900

    [MEDI-119] refactor: refactor error handler

--- src/main/java/com/my_medi/domain/consultationRequest/exception/ConsultationRequestHandler.java
+++ src/main/java/com/my_medi/domain/consultationRequest/exception/ConsultationRequestHandler.java
@@ -13,6 +13,23 @@ public class ConsultationRequestHandler extends GeneralException {
 
     public static final GeneralException REQUEST_STATUS_MISMATCH = new ConsultationRequestHandler(ConsultationRequestErrorStatus.STATUS_MISMATCH);
 
+    public static final GeneralException CONSULTATION_LIMIT_EXCEEDED = new ConsultationRequestHandler(ConsultationRequestErrorStatus.CONSULTATION_LIMIT_EXCEEDED);
+
+    public static final GeneralException ALREADY_PROCESSED_CONSULTATION = new ConsultationRequestHandler(ConsultationRequestErrorStatus.ALREADY_PROCESSED_CONSULTATION);
+
+    public static final GeneralException REQUEST_ONLY_CAN_BE_TOUCHED_BY_USER = new ConsultationRequestHandler(ConsultationRequestErrorStatus.REQUEST_ONLY_CAN_BE_TOUCHED_BY_USER);
+
+    public static final GeneralException EXPERT_MISMATCH = new ConsultationRequestHandler(ConsultationRequestErrorStatus.EXPERT_MISMATCH);
+
+    public static final GeneralException DUPLICATED_CONSULTATION = new ConsultationRequestHandler(ConsultationRequestErrorStatus.DUPLICATED_CONSULTATION);
+
+    public static final GeneralException INVALID_REQUEST_STATUS = new ConsultationRequestHandler(ConsultationRequestErrorStatus.INVALID_REQUEST_STATUS);
+
+
+
+
+
+
 
 
     public ConsultationRequestHandler(BaseErrorCode code) {
