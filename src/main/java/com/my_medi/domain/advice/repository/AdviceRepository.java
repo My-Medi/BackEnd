f0c513d80617db3e1ae113399cd32ed4c8ddf7bf
Pyun Seon a <139514905+Seona12@users.noreply.github.com>
2025-08-22 15:29:04 +0900

    [MEDI-122] refactor-expert-account-delete (#133)
    * [MEDI-122] add delete
    
    * [MEDI-122] fix pr

--- src/main/java/com/my_medi/domain/advice/repository/AdviceRepository.java
+++ src/main/java/com/my_medi/domain/advice/repository/AdviceRepository.java
@@ -4,6 +4,9 @@ import com.my_medi.domain.advice.entity.Advice;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
@@ -11,4 +14,8 @@ public interface AdviceRepository extends JpaRepository<Advice, Long> {
     Page<Advice> findAllByUserId(Long userId, Pageable pageable);
     Page<Advice> findAllByExpertIdAndUserId(Long expertId, Long userId, Pageable pageable);
     Optional<Advice> findTop1ByUserIdOrderByCreatedDateDescIdDesc(Long userId);
+
+    @Modifying(clearAutomatically = true, flushAutomatically = true)
+    @Query("delete from Advice a where a.expert.id = :expertId")
+    void deleteAllByExpertId(@Param("expertId") Long expertId);
 }
